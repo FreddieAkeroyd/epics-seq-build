fd9a9b195f245475f02860c0569f9ae4da549b76
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2017-01-22 21:58:34 +0000

    Split off loop from if - command expansion issue

--- fixrepos.bat
+++ fixrepos.bat
@@ -6,6 +6,8 @@ set "MYBASE=%~dp0epics-base"
 set CYGPATH_EXE=c:\cygwin64\bin\cygpath.exe
 if "%EPICS_HOST_ARCH:~0,6%" == "cygwin" (
     for /F "usebackq" %%I in (`%CYGPATH_EXE% %MYBASE%`) do SET MYBASECYG=%%I
+)
+if "%EPICS_HOST_ARCH:~0,6%" == "cygwin" (
     echo EPICS_BASE=%MYBASECYG%>configure\EPICS_BASE.%EPICS_HOST_ARCH%
     echo EPICS_BASE=%MYBASECYG%>>seq\configure\RELEASE
 	rem if you need to add packages, i think this is the way
