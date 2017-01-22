6a2e89578904f5eeb34a5cbacdac05cda22cec52
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2017-01-22 22:14:37 +0000

    Try not binding to loopback

--- fixrepos.bat
+++ fixrepos.bat
@@ -4,9 +4,12 @@ copy /y files\epics-startup-win32.bat epics-base\startup\win32.bat
 
 set "MYBASE=%~dp0epics-base"
 set CYGPATH_EXE=c:\cygwin64\bin\cygpath.exe
+
+REM need to do this is separate secion or get expansion issues
 if "%EPICS_HOST_ARCH:~0,6%" == "cygwin" (
     for /F "usebackq" %%I in (`%CYGPATH_EXE% %MYBASE%`) do SET MYBASECYG=%%I
 )
+
 if "%EPICS_HOST_ARCH:~0,6%" == "cygwin" (
     echo EPICS_BASE=%MYBASECYG%>configure\EPICS_BASE.%EPICS_HOST_ARCH%
     echo EPICS_BASE=%MYBASECYG%>>seq\configure\RELEASE
--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -o errexit
-export EPICS_CA_ADDR_LIST=127.0.0.1
-export EPICS_CA_AUTO_ADDR_LIST=NO
+# seem to give CAS ioctl errors?
+#export EPICS_CA_ADDR_LIST=127.0.0.1
+#export EPICS_CA_AUTO_ADDR_LIST=NO
 cd $1
 cd seq
 make -s runtests
