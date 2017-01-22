761b434057fb8ada4a9f2a3fe42bdf195876a223
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2017-01-22 01:00:13 +0000

    Set make SHELL

--- build.bat
+++ build.bat
@@ -3,6 +3,8 @@ setlocal
 call epics-base\startup\win32.bat
 set "PATH=%~dp0utils\make_v4;%~dp0utils\re2c;c:\strawberry\perl\bin;%PATH%"
 set "MYBASE=%~dp0epics-base"
+set "SHELL=%COMSPEC%"
+set "MAKESHELL=%COMSPEC%"
 echo EPICS_BASE=%MYBASE:\=/%>configure\EPICS_BASE.%EPICS_HOST_ARCH%
 echo EPICS_BASE=%MYBASE:\=/%>>seq\configure\RELEASE
 cd epics-base
--- runtests.bat
+++ runtests.bat
@@ -2,6 +2,8 @@
 setlocal
 call epics-base\startup\win32.bat
 set "PATH=%~dp0utils\make_v4;c:\strawberry\perl\bin;%PATH%"
+set "SHELL=%COMSPEC%"
+set "MAKESHELL=%COMSPEC%"
 cd seq
 set EPICS_CA_ADDR_LIST=127.0.0.1
 set EPICS_CA_AUTO_ADDR_LIST=NO
