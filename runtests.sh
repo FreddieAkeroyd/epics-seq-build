85429e75d45bb836114cc64fcd2300bf12c241e4
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2017-01-22 22:30:11 +0000

    Add a beacon address

--- runtests.sh
+++ runtests.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -o errexit
 # seem to give CAS ioctl errors?
-#export EPICS_CA_ADDR_LIST=127.0.0.1
-#export EPICS_CA_AUTO_ADDR_LIST=NO
+export EPICS_CA_ADDR_LIST=127.0.0.1
+export EPICS_CA_AUTO_ADDR_LIST=NO
+export EPICS_CA_BEACON_ADDR_LIST=127.255.255.255 
+export EPICS_CA_AUTO_BEACON_ADDR_LIST=NO
 cd $1
 cd seq
 make -s runtests
