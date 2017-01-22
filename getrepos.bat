2fc232ccd9cd1287318d47945cf93cfa63b9a0c6
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2017-01-22 00:25:25 +0000

    Pull seq 2.1.20 explicitly

--- getrepos.bat
+++ getrepos.bat
@@ -2,9 +2,6 @@
 setlocal
 set "PATH=C:\Program Files\Git\bin;%~dp0utils\darcs;%PATH%"
 git config --global core.autocrlf true
-git clone https://github.com/epics-base/epics-base
-cd epics-base
-git checkout R3.14.12.6
-cd ..
+git clone --quiet --branch=R3.14.12.6 --single-branch https://github.com/epics-base/epics-base
 rem curl -fsS -o epics_base.tar.gz https://www.aps.anl.gov/epics/download/base/baseR3.14.12.6.tar.gz
-darcs get --lazy http://www-csr.bessy.de/control/SoftDist/sequencer/repo/branch-2-2 seq
+darcs get --quiet --lazy --tag=R2-1-20 http://www-csr.bessy.de/control/SoftDist/sequencer/repo/branch-2-1 seq
