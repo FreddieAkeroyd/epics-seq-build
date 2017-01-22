5a163c681d8dd3d543811c51d7d63ce345358216
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2017-01-22 01:08:46 +0000

    Rename make to make4

--- build.bat
+++ build.bat
@@ -3,14 +3,17 @@ setlocal
 call epics-base\startup\win32.bat
 set "PATH=%~dp0utils\make_v4;%~dp0utils\re2c;c:\strawberry\perl\bin;%PATH%"
 set "MYBASE=%~dp0epics-base"
+REM maybe needed because MinGW is installed too?
 set "SHELL=%COMSPEC%"
 set "MAKESHELL=%COMSPEC%"
+
 echo EPICS_BASE=%MYBASE:\=/%>configure\EPICS_BASE.%EPICS_HOST_ARCH%
 echo EPICS_BASE=%MYBASE:\=/%>>seq\configure\RELEASE
+
 cd epics-base
-make
+make4
 if %errorlevel% neq 0 exit /b %errorlevel% 
 cd ..\seq
-make
+make4
 if %errorlevel% neq 0 exit /b %errorlevel% 
 cd ..
--- runtests.bat
+++ runtests.bat
@@ -2,11 +2,14 @@
 setlocal
 call epics-base\startup\win32.bat
 set "PATH=%~dp0utils\make_v4;c:\strawberry\perl\bin;%PATH%"
+REM maybe needed because MinGW is installed too?
 set "SHELL=%COMSPEC%"
 set "MAKESHELL=%COMSPEC%"
-cd seq
+REM explicitly setting these avoids warnings during ioc tests
 set EPICS_CA_ADDR_LIST=127.0.0.1
 set EPICS_CA_AUTO_ADDR_LIST=NO
-make -s runtests
+
+cd seq
+make4 -s runtests
 if %errorlevel% neq 0 exit /b %errorlevel% 
 cd ..
