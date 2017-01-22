802a780c1ad299eff65ad8c78c88fd9200a8bbdc
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2017-01-22 00:34:57 +0000

    Fix EPICS_BASE in seq RELEASE

--- build.bat
+++ build.bat
@@ -4,6 +4,7 @@ call epics-base\startup\win32.bat
 set "PATH=%~dp0utils\make_v3;%~dp0utils\re2c;c:\strawberry\perl\bin;%PATH%"
 set "MYBASE=%~dp0epics-base"
 echo EPICS_BASE=%MYBASE:\=/%>configure\EPICS_BASE.%EPICS_HOST_ARCH%
+echo EPICS_BASE=%MYBASE:\=/%>>seq\configure\RELEASE
 cd epics-base
 make
 if %errorlevel% neq 0 exit /b %errorlevel% 
