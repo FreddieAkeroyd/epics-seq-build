bf5f67559aa675de0a52c5586f5374f21203ec46
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2017-01-23 11:02:05 +0000

    Install libreadline-devel on cygwin

--- fixrepos.bat
+++ fixrepos.bat
@@ -6,8 +6,12 @@ set "MYBASE=%~dp0epics-base"
 
 if "%EPICS_HOST_ARCH:~0,13%" == "cygwin-x86_64" (
     set CYGPATH_EXE=c:\cygwin64\bin\cygpath.exe
+    set CYGSETUP=c:\cygwin64\setup-x86_64.exe
+    set CYGDIR=c:\cygwin64
 ) else (
     set CYGPATH_EXE=c:\cygwin\bin\cygpath.exe
+    set CYGSETUP=c:\cygwin\setup-x86.exe
+    set CYGDIR=c:\cygwin
 )
 
 REM need to do this is separate secion or get expansion issues
@@ -18,11 +22,10 @@ if "%EPICS_HOST_ARCH:~0,6%" == "cygwin" (
 if "%EPICS_HOST_ARCH:~0,6%" == "cygwin" (
     echo EPICS_BASE=%MYBASECYG%>configure\EPICS_BASE.%EPICS_HOST_ARCH%
     echo EPICS_BASE=%MYBASECYG%>>seq\configure\RELEASE
-	rem if you need to add packages, i think this is the way
-    rem c:\cygwin64\setup-x86_64.exe -qnNdO -R C:/cygwin64 -s http://cygwin.mirror.constant.com -l C:/cygwin64/var/cache/setup -P somepackagename
+    REM if you need to add packages, i think this is the way
+    %CYGSETUP% -qnNdO -R %CYGDIR:\=/% -s http://cygwin.mirror.constant.com -l %CYGDIR:\=/%/var/cache/setup -P libreadline-devel
 ) else (
     echo EPICS_BASE=%MYBASE:\=/%>configure\EPICS_BASE.%EPICS_HOST_ARCH%
     echo EPICS_BASE=%MYBASE:\=/%>>seq\configure\RELEASE
 )
-
 
