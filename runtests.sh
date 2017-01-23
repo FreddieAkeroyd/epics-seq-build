f38c5be82f8979a2fa21dfbfdc13522a4b5ed732
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2017-01-23 11:23:17 +0000

    Set local rather than loopback address for tests

--- runtests.sh
+++ runtests.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -o errexit
+MYIP=`ipconfig|grep IPv4|awk -F: '{print $2}' | sed -s 's/ //g'`
 # seem to give CAS ioctl errors?
-export EPICS_CA_ADDR_LIST=127.0.0.1
+export EPICS_CA_ADDR_LIST=$MYIP
 export EPICS_CA_AUTO_ADDR_LIST=NO
-export EPICS_CA_BEACON_ADDR_LIST=127.255.255.255
+#export EPICS_CA_BEACON_ADDR_LIST=127.255.255.255
 export EPICS_CA_AUTO_BEACON_ADDR_LIST=NO
 cd $1
 cd seq
