98d443bf00660e00ac1bee4be95bf4dee10fac09
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2017-01-23 11:46:19 +0000

    Run tests non silently

--- runtests.sh
+++ runtests.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 set -o errexit
 MYIP=`ipconfig|grep IPv4|awk -F: '{print $2}' | sed -s 's/ //g'`
-# seem to give CAS ioctl errors?
+# loopback seems to give CAS ioctl errors?
 export EPICS_CA_ADDR_LIST=$MYIP
 export EPICS_CA_AUTO_ADDR_LIST=NO
 #export EPICS_CA_BEACON_ADDR_LIST=127.255.255.255
 export EPICS_CA_AUTO_BEACON_ADDR_LIST=NO
+echo EPICS_CA_ADDR_LIST=$EPICS_CA_ADDR_LIST
 cd $1
 cd seq
 make -s runtests
