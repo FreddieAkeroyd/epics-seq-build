22baf5fa7fee02800c43c791d45bb2eb65bab85b
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2017-01-24 08:52:08 +0000

    Make sure only one IP address

--- runtests.sh
+++ runtests.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 set -o errexit
-MYIP=`ipconfig|grep IPv4|awk -F: '{print $2}' | sed -s 's/ //g'`
+# get one IP address
+MYIP=`ipconfig|grep IPv4|awk -F: '{print $2}'|head -1| sed -s 's/ //g'`
 # loopback seems to give CAS ioctl errors?
+#MYIP=127.0.0.1
+#
 export EPICS_CA_ADDR_LIST=$MYIP
 export EPICS_CA_AUTO_ADDR_LIST=NO
 #export EPICS_CA_BEACON_ADDR_LIST=127.255.255.255
+#export EPICS_CA_BEACON_ADDR_LIST=$EPICS_CA_ADDR_LIST
 export EPICS_CA_AUTO_BEACON_ADDR_LIST=NO
 echo EPICS_CA_ADDR_LIST=$EPICS_CA_ADDR_LIST
 cd $1
