8b00d9a51546592a34e798885f27c3dea391b82c
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2017-01-24 11:50:20 +0000

    Launch caRepeater explicitly

--- runtests.sh
+++ runtests.sh
@@ -8,10 +8,11 @@ MYIP=`ipconfig|grep IPv4|awk -F: '{print $2}'|head -1| sed -s 's/ //g'`
 export EPICS_CA_ADDR_LIST=$MYIP
 export EPICS_CA_AUTO_ADDR_LIST=NO
 #export EPICS_CA_BEACON_ADDR_LIST=127.255.255.255
-#export EPICS_CA_BEACON_ADDR_LIST=$EPICS_CA_ADDR_LIST
+#export EPICS_CA_BEACON_ADDR_LIST=${EPICS_CA_ADDR_LIST}
 export EPICS_CA_AUTO_BEACON_ADDR_LIST=NO
-echo EPICS_CA_ADDR_LIST=$EPICS_CA_ADDR_LIST
+echo EPICS_CA_ADDR_LIST=${EPICS_CA_ADDR_LIST}
 cd $1
+nohup ./epics-base/bin/${EPICS_HOST_ARCH}/caRepeater.exe < /dev/null > /dev/null 2>&1 &
 cd seq
 nohup make runtests < /dev/null
 cd ..
