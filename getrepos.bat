2a25b7fd36271834cc14fd2adc59534aa0cacc32
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2017-02-20 19:25:53 +0000

    Add git fallback if darcs fails

--- getrepos.bat
+++ getrepos.bat
@@ -13,14 +13,28 @@ if exist "epics-base" (
 @echo Getting EPICS %EPICS_VER%
 git clone %CLONEOPTS% --quiet --branch=R%EPICS_VER% --single-branch https://github.com/epics-base/epics-base
 REM curl -fsS -o epics_base.tar.gz https://www.aps.anl.gov/epics/download/base/baseR3.14.12.6.tar.gz
+if %errorlevel% neq 0 exit /b %errorlevel%
 :SEQ
 if exist "seq" (
     rd /q /s seq
 )
 if "%SEQ_VER%" == "" (
-    @echo Getting latest SEQ %SEQ_BRANCH% branch
+    @echo Getting latest SEQ %SEQ_BRANCH% branch - darcs
     darcs get --quiet --lazy http://www-csr.bessy.de/control/SoftDist/sequencer/repo/branch-%SEQ_BRANCH% seq
 ) else (
-    @echo Getting SEQ version %SEQ_VER% from %SEQ_BRANCH% branch
+    @echo Getting SEQ version %SEQ_VER% from %SEQ_BRANCH% branch - darcs
     darcs get --quiet --lazy --tag=R%SEQ_VER% http://www-csr.bessy.de/control/SoftDist/sequencer/repo/branch-%SEQ_BRANCH% seq
+)
+if %errorlevel% neq 0 (
+    @echo DARCS failed - retrying using git
+    if exist "seq" (
+        rd /q /s seq
+    )
+    if "%SEQ_VER%" == "" (
+        @echo Getting latest SEQ %SEQ_BRANCH% branch - git
+		git clone %CLONEOPTS% --quiet --branch=master --single-branch http://www-csr.bessy.de/control/SoftDist/sequencer/repo/branch-%SEQ_BRANCH%.git seq
+    ) else (
+        @echo Getting SEQ version %SEQ_VER% from %SEQ_BRANCH% branch - git
+		git clone %CLONEOPTS% --quiet --branch=R%SEQ_VER% --single-branch http://www-csr.bessy.de/control/SoftDist/sequencer/repo/branch-%SEQ_BRANCH%.git seq
+    )
 )
